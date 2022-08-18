54b7494216cd54435c6116c8a742f1b91e7d0cfa
Jameson Taylor <jameson@ionic.io>
2022-08-18 09:58:08 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pull-to-refresh',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
